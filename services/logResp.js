364afdd7dd570fdd7ba9c116a5a5d834c5618f82
Zvantsou <m.zvantsov@gmail.com>
2022-12-02 15:47:20 +0300

    with_chulk_lib

---
+++ services/logResp.js
@@ -0,0 +1,23 @@
+import chalk from "chalk"
+import dedent from "dedent";
+
+
+const printError = (error) => {
+    console.log(chalk.bgRed(' ERROR ') + error);
+}
+
+const printMessage = (message) => {
+    console.log(chalk.bgGreen(' SUCCESS ') + message);
+}
+
+const printHelp = () => {
+    console.log(
+        dedent(chalk.bgYellow(' Help ') + '\n' +
+        'Без параметров - вывод погоды ' + '\n' +
+		'-s [CITY] для установки города ' + '\n' +
+		'-h для вывода помощи ' + '\n' +
+		'-t [API_KEY] для сохранения токена')
+    );
+}
+
+export { printError, printMessage, printHelp };
--- weather.js
+++ weather.js
@@ -1,9 +1,10 @@
 import {getArgs} from './helpers/args.js';
+import { printHelp } from './services/logResp.js';
 const startCli = () => {
     const args = getArgs(process.argv);
     console.log(args);
 	if (args.h) {
-		console.log(args.h)
+		printHelp();
 	}
 	if (args.s) {
 		console.log(args.s)
